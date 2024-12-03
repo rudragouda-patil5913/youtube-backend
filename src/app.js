be982bef6dca54551470e84000bf06eaa16b97af
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2024-12-03 16:43:09 +0530

    Server is running all time test

--- src/app.js
+++ src/app.js
@@ -15,4 +15,15 @@ app.use(express.json({ limit: "16kb" })); // we can extend the limit
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // static files like images css
 
+//import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.use("*", (req, res) => {
+  res.json({
+    msg: "Something went Wrong",
+  });
+});
+
 export { app };
