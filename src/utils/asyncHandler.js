ae5c17797a98c9e2d9708e8bb53889c74cff2535
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2024-12-03 13:50:24 +0530

    Standard asyncHandler ApiResponse ApiError

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (responseHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(responseHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export { asyncHandler };
