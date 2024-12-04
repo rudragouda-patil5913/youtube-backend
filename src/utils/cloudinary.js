e448e615f6a9c02bfbf01a1e5dbbffcccd31c600
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2024-12-04 10:45:43 +0530

    multer cloudinary pre-hook methods

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File src:" + response.url);
+    // once uploaded delete it from server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log("Error in cloudinary", error);
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    console.log("Deleted from cloudinary. Public Id:", publicId);
+  } catch (error) {
+    console.log("Error dedleting from cloudinary", error);
+    return null;
+  }
+};
+
+
+export { uploadOnCloudinary, deleteFromCloudinary };
